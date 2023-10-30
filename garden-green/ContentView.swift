6fda365eb521b11c4e748e9dfaac405877157969
David Grammatico <david.lugdunum@gmail.com>
2023-10-30 07:38:15 +0100

    Initial Commit

---
+++ garden-green/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  garden-green
+//
+//  Created by David Grammatico on 30/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ garden-green/garden_greenApp.swift
@@ -0,0 +1,17 @@
+//
+//  garden_greenApp.swift
+//  garden-green
+//
+//  Created by David Grammatico on 30/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct garden_greenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
